5f1169d478a813338221aca41f4bdffc61d143cd
Krishi-mehta <mehtakrishi87@gmail.com>
2025-07-10 16:57:48 +0530

    Nextjs project commit

---
+++ src/app/admin/actions.js
@@ -0,0 +1,50 @@
+"use server";
+import { auth } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs/server";
+import { Roles } from "utils/roles";
+import { revalidatePath } from "next/cache";
+
+export async function setRole(formData) {
+  const { sessionClaims } = await auth();
+
+  // Check that the user trying to set the role is an admin
+  if (sessionClaims?.metadata?.role !== "admin") {
+    throw new Error("Not Authorized");
+  }
+
+  const client = await clerkClient();
+  const id = formData.get("id");
+  const role = formData.get("role");
+
+  try {
+    await client.users.updateUser(id, {
+      publicMetadata: { role },
+    });
+    revalidatePath("/admin");
+  } catch {
+    throw new Error("Failed to set role");
+  }
+}
+
+export async function removeRole(formData) {
+  const { sessionClaims } = await auth();
+
+  if (sessionClaims?.metadata?.role !== Roles.ADMIN) {
+    throw new Error("Not Authorized");
+  }
+  if (sessionClaims?.metadata?.role !== "admin") {
+    throw new Error("Not Authorized");
+  }
+
+  const client = await clerkClient();
+  const id = formData.get("id");
+
+  try {
+    await client.users.updateUser(id, {
+      publicMetadata: { role: null },
+    });
+    revalidatePath("/admin");
+  } catch {
+    throw new Error("Failed to remove role");
+  }
+}
